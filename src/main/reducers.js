931b5bd16a4c884639c9e30b0468580f8ab23378
Felipe Campelo <fcampelocc@gmail.com>
2018-02-09 04:58:45 -0300

    add reducers tab

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import {reducer as toastrReducer} from 'react-redux-toastr';
 
 import BeerReducers from '../beer/BeerReducers';
+import TabReducers from '../common/tab/tabReducers';
 
 const rootReducers = combineReducers({
   beers: BeerReducers,
-  toastr: toastrReducer
+  toastr: toastrReducer,
+  tab: TabReducers
 });
 
 export default rootReducers;
