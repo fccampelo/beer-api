ce56be12cf41c72e354aaab89be476820c65f132
Felipe Campelo <fcampelocc@gmail.com>
2018-02-09 04:54:31 -0300

    adding toastr module and selectedTab action

--- src/beer/BeerActions.js
+++ src/beer/BeerActions.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import {toastr} from 'react-redux-toastr';
+import { toastr } from 'react-redux-toastr';
 
 import { BEER_ACTION, BASE_URL } from '../config/const';
+import { selectedTab } from '../common/tab/tabActions'
 
 export function getListBeers() {
   const request = axios.get(`${BASE_URL}?page=1&per_page=80`);
@@ -12,7 +13,6 @@ export function getListBeers() {
 };
 
 export function getFilterBeers(filter) {
- 
   const request = axios.get(`${BASE_URL}?beer_name=${filter}`);
   request
     .then(res =>  toastr.success('Sucesso', 'operação realizada com sucesso'))  
@@ -21,11 +21,11 @@ export function getFilterBeers(filter) {
     type: BEER_ACTION.FILTER_NAME,
     payload: request
   }
-  
 };
 
 export function getBeer(id)  {
   const request = axios.get(`${BASE_URL}/${id}`)
+  selectedTab('ingrediente');
   return {
     type: BEER_ACTION.BEER,
     payload: request
