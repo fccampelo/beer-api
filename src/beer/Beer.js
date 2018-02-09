0888368b242775fe413784476b531a55902ca40a
Felipe Campelo <fcampelocc@gmail.com>
2018-02-09 04:53:24 -0300

    adding tab component

--- src/beer/Beer.js
+++ src/beer/Beer.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import Tabs from '../common/tab/tabs';
 import Card from '../common/ui/card';
+import Wiget from '../common/ui/widget';
+import TabHeader from '../common/tab/tabHeader';
+import TabsHeader  from '../common/tab/tabsHeader';
+import TabContent from '../common/tab/tabContent';
+import TabsContent from '../common/tab/tabsContent';
 import { getBeer } from './BeerActions';
+import ListComponent from '../common/list/list';
 
 class Beer extends Component {
   
@@ -17,17 +24,39 @@ class Beer extends Component {
       <div>
         {this.props.beer.map((data, i) => {
           return (
-            <Card title={data.name} key={i} description={data.description} img={data.image_url} />
+            <div className="row">
+               <div className="col-md-12" style={{ position: 'absolut' }}>
+                <Wiget className="col-md-2" info={'Nível de PH'} value={data.ph} icon={'line-chart'} />
+                <Wiget className="col-md-2" info={'Nível de Atenuação'} value={data.attenuation_level}  icon={'fire'}/>
+                <Wiget className="col-md-2" info={'Nome da cerveja'} value={data.name}  icon={'tag'}/>
+                <Wiget className="col-md-2" info={'1ª Fabricação'} value={data.first_brewed}  icon={'calendar'}/>
+              </div>
+              <div className="col-md-6">  
+                 <Card title={data.name} key={i} description={data.description} img={data.image_url} />
+              </div>
+              <div className="col-sm-12 col-md-6"> 
+                <Tabs style={{margin: '0 auto'}}>
+                  <TabsHeader >
+                    <TabHeader  label='Quantidade Malt' icon='bars' target='ingrediente' />
+                    <TabHeader  label='Lúpulo da Cerveja' icon='bars' target='sugestao' />
+                  </TabsHeader>
+                  <TabsContent>
+                    <TabContent id='ingrediente'> <ListComponent title={'Malt'} info={data.ingredients.malt} key={data.id}/> </TabContent>
+                    <TabContent id='sugestao'>  <ListComponent title={'Lúpulo'} info={data.ingredients.hops} key={data.id}/> </TabContent>
+                  </TabsContent>
+                </Tabs>
+              </div>
+            </div>
           )
         })}    
       </div>  
     )
   }
+};
 
-}
 const mapStateToProps = state => ({
   beer : state.beers.beer
 });
-const mapDispatchToProps = dispatch => bindActionCreators({getBeer}, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ getBeer }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Beer);
