1aa35f07fd5cf7dc752f9da7d937f4e0582c06ba
Felipe Campelo <fcampelocc@gmail.com>
2018-02-09 04:57:19 -0300

    create react component Card

--- src/common/ui/card.js
+++ src/common/ui/card.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default props => (
-  <div className="card">
-    <img className="card-img-top" src={props.img}  alt={props.title} />
-    <div className="card-block">
-      <h4 className="card-title"> <b>{props.title}</b></h4>
-      <p className="card-text" style={{padding: '20px'}}>{props.description}</p>
-      <a href="#" className="btn btn-primary">Voltar para a lista</a>
-    </div>
-  </div>
-);
+class Card extends Component {
+ 
+  render() {
+    return (
+      <div className="col-sd-12">
+        <div className="card">
+          <div className="card-content">
+            <img className="card-img-top" src={this.props.img}  alt={this.props.title} />
+            <div className="card-block">
+              <h4 className="card-title"><b>{this.props.title}</b></h4>
+              <p className="card-text" style={{padding: '20px'}}>{this.props.description}</p>
+              <a href="#" className="btn btn-primary card-btn">Voltar para a lista</a>
+            </div>     
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card
